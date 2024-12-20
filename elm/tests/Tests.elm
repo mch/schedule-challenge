0c3a7e6e8071eaaf44e12321e9fc87e9e20c3905
Matt Hughes <matt.c.hughes@gmail.com>
2024-12-20 12:21:37 -0700

    refactor a name

--- elm/tests/Tests.elm
+++ elm/tests/Tests.elm
@@ -4,23 +4,26 @@ import Expect
 import Test exposing (Test, describe, test)
 
 
-filterEvents events query = 
-     List.filter (\appleSauce -> String.contains query appleSauce) events
+filterEvents events query =
+    List.filter (\event -> String.contains query event) events
+
 
 suite : Test
 suite =
     describe "Schedule Logic"
-        [ test "we can filter the schedule to match input" 
-            <| \_ -> Expect.equal 
-                (filterEvents ["Eat breakfast", "Workout"] "Eat") 
-                ["Eat breakfast"]
-        , test "if nothing matches, nothing is returned"
-        <| \_ -> Expect.equal 
-                (filterEvents ["Eat breakfast", "Workout"] "drink coffee") 
-                []
-        , test "if the query matches multiple items in the list return all the items that contains the query"
-        <| \_ -> Expect.equal 
-                (filterEvents ["Eat breakfast", "Workout", "Eat lunch"] "Eat") 
-                ["Eat breakfast", "Eat lunch"]
+        [ test "we can filter the schedule to match input" <|
+            \_ ->
+                Expect.equal
+                    (filterEvents [ "Eat breakfast", "Workout" ] "Eat")
+                    [ "Eat breakfast" ]
+        , test "if nothing matches, nothing is returned" <|
+            \_ ->
+                Expect.equal
+                    (filterEvents [ "Eat breakfast", "Workout" ] "drink coffee")
+                    []
+        , test "if the query matches multiple items in the list return all the items that contains the query" <|
+            \_ ->
+                Expect.equal
+                    (filterEvents [ "Eat breakfast", "Workout", "Eat lunch" ] "Eat")
+                    [ "Eat breakfast", "Eat lunch" ]
         ]
-
