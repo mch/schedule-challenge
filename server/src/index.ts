f5a401a663f3850744cf4a3beba88650e6b561a4
Tony Vo <ttrung.vo@gmail.com>
2025-05-10 08:14:22 -0600

    add express hello world app

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,3 +1,15 @@
 export function foo() {}
 
 export class Bar {}
+
+import express, { Request, Response } from 'express';
+const app = express()
+const port = 3000
+
+app.get('/', (req: Request, res: Response) => {
+    res.send('Hello World!')
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
