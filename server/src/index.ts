72f1d485bb607c9233d1b3d4f728c53f20dd6d89
Tony Vo <ttrung.vo@gmail.com>
2025-05-31 08:25:02 -0600

    add profiler

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,13 +1,17 @@
 export function foo() {}
 
 export class Bar {}
-
+import { profile } from './profiler.js';
 import express, { Request, Response } from 'express';
 const app = express()
 const port = 3000
 
 app.get('/', (req: Request, res: Response) => {
     res.send('Hello World!')
+})
+
+app.get('/profile', async (req: Request, res: Response) => {
+    res.json(await profile(20));
 })
 
 app.get('/fibonacci/:n', (req: Request, res: Response) => {
---
+++ server/src/profiler.ts
@@ -0,0 +1,14 @@
+import {Session} from 'node:inspector/promises'
+export async function profile(durationInSecond: number) {
+    const session = new Session();
+    session.connect();
+    await session.post('Profiler.enable');
+    await session.post('Profiler.start');
+
+    await new Promise((resolve) => {
+        setTimeout(resolve, durationInSecond*1000);
+    });
+
+    const result = await session.post('Profiler.stop');
+    return JSON.stringify(result.profile);
+}
