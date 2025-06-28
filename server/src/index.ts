ddded3062615e38b89818a92d839418fef393e94
Tony Vo <ttrung.vo@gmail.com>
2025-06-28 08:04:10 -0600

    add dynamic logger

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,4 +1,4 @@
-import { configurableSampler } from "./instrumentation";
+import { configurableSampler } from "./instrumentation.js";
 
 export function foo() {}
 
@@ -7,16 +7,26 @@ import { profile } from './profiler.js';
 import express, { Request, Response } from 'express';
 const app = express()
 const port = 3000
+import winston from 'winston';
+
+const logger = winston.createLogger({
+    level: 'info',
+    format: winston.format.json(),
+    transports: [new winston.transports.Console()],
+});
 import { TraceAPI } from '@opentelemetry/api';
 import {NodeTracerProvider} from "@opentelemetry/sdk-trace-node";
+import {inspect} from "node:util";
 
 app.get('/', async (req: Request, res: Response) => {
     const fetchNodeDocResult = await fetch('https://nodejs.org/api/documentation.json');
+    let data = {};
     if (fetchNodeDocResult.ok) {
-        const data = await fetchNodeDocResult.json();
-        console.log(data);
+        data = await fetchNodeDocResult.json();
+        logger.info(data);
     }
 
+    logger.info(`response=${inspect(req, {depth: 3})}`);
     res.send('Hello World!')
 })
 
@@ -26,10 +36,12 @@ app.get('/profile', async (req: Request, res: Response) => {
 
 app.get('/toggle-tracing', (req: Request, res: Response) => {
     configurableSampler.enabled = !configurableSampler.enabled;
+    logger.level = 'error';
     res.json({ tracingEnabled: configurableSampler.enabled });
 });
 
 app.get('/fibonacci/:n', (req: Request, res: Response) => {
+    logger.info(`req=${inspect(req, {depth: 3})}`);
     const n = parseInt(req.params.n, 10);
     if (isNaN(n) || n < 0) {
         res.status(400).json({ error: 'Invalid input' });
@@ -41,6 +53,7 @@ app.get('/fibonacci/:n', (req: Request, res: Response) => {
         return fibonacci(num - 1) + fibonacci(num - 2);
     }
     const result = fibonacci(n);
+    logger.info(`result=${inspect(result, {depth: 3})}`);
     res.json({ n, fibonacci: result });
 });
 
